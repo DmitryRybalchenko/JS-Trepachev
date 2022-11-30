a1004a5ccea20f579a7bc10f234ac11b2ec36360
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-01 09:25:46 +1000

    feat: add 1, 2, 3

--- jsPmBsSC/script.js
+++ jsPmBsSC/script.js
@@ -1,6 +1,19 @@
 "use strict";
 
-let name = 'Dima';
-let lastName = 'Rybalchenko';
+// №1⊗jsPmBsSC
+// Создайте переменную str и присвойте ей строку '!!!'. Выведите значение этой переменной на экран.
+// №2⊗jsPmBsSC
+// Создайте переменную с текстом 'java' и переменную с текстом 'script'. С помощью этих переменных и операции сложения строк выведите на экран строку 'javascript'.
+// №3⊗jsPmBsSC
+// Создайте переменную с текстом 'hello' и переменную с текстом 'world'. С помощью этих переменных и операции сложения строк выведите на экран строку 'hello world'.
 
-alert(name + lastName);
+// let str = '!!!';
+// alert('str');
+
+// let str = 'jave';
+// let text = 'script';
+// alert(str + text)
+
+let hell = 'hello';
+let world = ' world';
+alert(hell + world);
