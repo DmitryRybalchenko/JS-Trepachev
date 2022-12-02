9ec8721e45c37e23a5bb5eaa53a2bcacc28fb3f7
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-02 18:11:11 +1000

    feat: add full

--- jsPmBsCLT/script.js
+++ jsPmBsCLT/script.js
@@ -1,4 +1,148 @@
 "use strict";
 
-let name = "Дмитрий";
-alert(name.length);
+// №1⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(3);
+// alert(test);
+//true
+
+// №2⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(0);
+// alert(test);
+//false
+
+// №3⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(-1);
+// alert(test);
+//true
+
+// №4⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(-0);
+// alert(test);
+//false
+
+// №5⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(+0);
+// alert(test);
+//false
+
+// №6⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean('abc');
+// alert(test);
+//true
+
+// №7⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean('');
+// alert(test);
+//false
+
+// №8⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean('0');
+// alert(test);
+//true
+
+// №9⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(true);
+// alert(test);
+//true
+
+// №10⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(false);
+// alert(test);
+//false
+
+// №11⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean('true');
+// alert(test);
+//true
+
+// №12⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean('false');
+// alert(test);
+//true
+
+// №13⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(null);
+// alert(test);
+//false
+
+// №14⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean('null');
+// alert(test);
+//true
+
+// №15⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(undefined);
+// alert(test);
+//false
+
+// №16⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean('undefined');
+// alert(test);
+//true
+
+// №17⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(NaN);
+// alert(test);
+//false
+
+// №18⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean('NaN');
+// alert(test);
+//true
+
+// №19⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(3 * 'abc');
+// alert(test);
+//false
+
+// №20⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(Infinity);
+// alert(test);
+//true
+
+// №21⊗jsPmBsCLT
+// Не запуская код, определите, что выведется на экран:
+
+// let test = Boolean(1 / 0);
+// alert(test);
+//true
