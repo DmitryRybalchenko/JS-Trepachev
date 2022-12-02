46b931d6181ce5ca540e737f46340992a96951ca
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-02 19:12:22 +1000

    feat: add full

--- jsPmBsLSCh/script.js
+++ jsPmBsLSCh/script.js
@@ -1,2 +1,17 @@
 "use strict";
 
+// №1⊗jsPmBsLSCh
+// Дана строка. Выведите на экран ее последний символ.
+
+// let str = 'Ghbdtn';
+// console.log(str[str.length - 1]);
+
+// №2⊗jsPmBsLSCh
+// Дана строка. Выведите на экран ее предпоследний символ.
+
+// let str = 'Ghbdtn';
+// console.log(str[str.length - 2]);
+
+// №3⊗jsPmBsLSCh
+// Дана строка. Выведите на экран ее предпредпоследний символ.
+// console.log(str[str.length - 3]);
