ffd059f914d2c34ad44328a1f080c27a035353f5
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-03 11:34:16 +1000

    fix: add full

---
+++ jsPmBsDW/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// №1⊗jsPmBsDW
+// Выведите в окно браузера какой-нибудь текст.
+// document.write('ghbdtn');
+
+// №2⊗jsPmBsDW
+// Выведите в окно браузера какой-нибудь текст, сделав его курсивным.
+// document.write('<i>Привет</i>');
+
+// №3⊗jsPmBsDW
+// Дана переменная:
+// let str = 'text';
+// Выведите текст из переменной в окно браузера так, чтобы он был курсивным.
+// document.write(`<i>${str}</i>`)
+
+// №4⊗jsPmBsDW
+// Выведите в окно браузера столбец чисел от 1 до 5.
+document.write(1 + '<br>');
+document.write(2 + '<br>');
+document.write(3 + '<br>');
+document.write(4 + '<br>');
+document.write(5 + '<br>');
