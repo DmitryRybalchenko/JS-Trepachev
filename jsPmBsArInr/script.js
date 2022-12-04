34006bc82fd40a90de66443c3c016cd20d400439
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-04 15:14:55 +1000

    feat: add full

--- jsPmBsArInr/script.js
+++ jsPmBsArInr/script.js
@@ -1,2 +1,12 @@
 "use strict";
 
+// №1⊗jsPmArInr
+// Создайте массив с числами. Выведите его на экран с помощью функции alert, а также выведите его в консоль с помощью console.log.
+// let arr = [1,2,3,4,5];
+// console.log(arr);
+
+// №2⊗jsPmArInr
+// Создайте массив со строками. Выведите его на экран с помощью функции alert, а также выведите его в консоль с помощью console.log.
+// let arr = ['ghbd', 'dfsdf', 'aaee', 'kdkk'];
+// alert(arr);
+// console.log(arr);
