d300ce54dd234951f571836bedab6b80e8efaec3
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-05 10:39:02 +1000

    feat: jsPmArMst finish

--- array/script.js
+++ array/script.js
@@ -43,3 +43,33 @@
 // let arr = ['a', 'b', 'c', 'd', 'e'];
 // delete arr[0];
 // console.log(arr.length, arr);
+
+// №1⊗jsPmArMst
+// Код должен вывести последний элемент массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length]);
+
+// №1⊗jsPmArMst
+// Код должен вывести последний элемент массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length - 1]);
+
+// №2⊗jsPmArMst
+// Код должен найти сумму элементов массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+// №3⊗jsPmArMst
+// Код должен вывести длину массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// №4⊗jsPmArMst
+// Код должен вывести длину массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
