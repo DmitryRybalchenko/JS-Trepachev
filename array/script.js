5be66efd032857d6aacb641b64948cb298aa8bb8
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-05 10:26:35 +1000

    feat: jsPmArVK

--- array/script.js
+++ array/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+// №1⊗jsPmArEPA
+// Заполните массив числами 1, 2 и 3.
+// let arr = [];
+// arr.push(1);
+// arr.push(2);
+// arr.push(3);
+// console.log(arr);
+
+// №2⊗jsPmArEPA
+// Пусть дан такой массив:
+
+// Добавьте ему в конец элементы 4 и 5.
+// let arr = [1, 2, 3];
+
+// arr.push(4);
+// arr.push(5);
+// console.log(arr);
