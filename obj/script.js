4905eaa66f86b18c7b67a394a058beef67b948db
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-05 10:41:18 +1000

    start: obj

---
+++ obj/script.js
@@ -0,0 +1,75 @@
+"use strict";
+
+// №1⊗jsPmArEPA
+// Заполните массив числами 1, 2 и 3.
+// let arr = [];
+// arr.push(1);
+// arr.push(2);
+// arr.push(3);
+// console.log(arr);
+
+// №2⊗jsPmArEPA
+// Пусть дан такой массив:
+
+// Добавьте ему в конец элементы 4 и 5.
+// let arr = [1, 2, 3];
+
+// arr.push(4);
+// arr.push(5);
+// console.log(arr);
+
+// №1⊗jsPmArVK
+// Дан следующий массив:
+// Дана также переменная:
+// Выведите на экран элемент, ключ которого хранится в переменной key.
+// let arr = ['a', 'b', 'c'];
+// let key = 2;
+
+// console.log(arr[key]);
+
+// №2⊗jsPmArVK
+// Дан следующий массив:
+// Даны также переменные:
+// Найдите сумму элементов, ключи которых хранятся в наших переменных.
+// let arr = [1, 2, 3, 4, 5];
+// let key1 = 1;
+// let key2 = 2;
+// let res = arr[key1] + arr[key2];
+// console.log(res);
+
+// №1⊗jsPmArOD
+// Дан массив:
+// Удалите из него два элемента. Проверьте, какое станет значение свойства length после этого.
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// delete arr[0];
+// console.log(arr.length, arr);
+
+// №1⊗jsPmArMst
+// Код должен вывести последний элемент массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length]);
+
+// №1⊗jsPmArMst
+// Код должен вывести последний элемент массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length - 1]);
+
+// №2⊗jsPmArMst
+// Код должен найти сумму элементов массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+// №3⊗jsPmArMst
+// Код должен вывести длину массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// №4⊗jsPmArMst
+// Код должен вывести длину массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
