b716c58929ab7cd63483a23da566c99bfb853453
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-09 12:32:12 +1000

    feat: switch-case end

--- switch-case/script.js
+++ switch-case/script.js
@@ -1,2 +1,42 @@
 "use strict";
 
+// let lang = 'ru';
+
+// switch (lang) {
+//     case 'ru': 
+//         console.log('рус');
+//     break;
+//     case 'en':
+//         console.log('анг');
+//     break;
+//     case 'de':
+//         console.log('нем');
+//     break;
+//     default:
+//         console.log('язык не поддерживается');
+//     break;
+// }   
+
+// let num = 1;
+// let res;
+
+// res = (num >= 0) ? 1 : 2;
+// console.log(res);
+
+// ⊗jsPmCdLOp
+// Пусть даны следующие переменные:
+// Используя оператор ==, узнайте, равны ли значения этих переменных или нет.
+
+// let a = 2 * (3 - 1);
+// let b = 6 - 2;
+
+// console.log(a == b);
+
+// №2⊗jsPmCdLOp
+// Пусть даны следующие переменные:
+// Используя оператор >, узнайте, больше ли переменная a, чем b.
+
+let a = 5 * (7 - 4);
+let b = 1 + 2 + 7;
+
+console.log(a > b);
