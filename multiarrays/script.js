2a93e95e705303e0e6d5c24eede4036f19ada2ef
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-16 11:11:56 +1000

    feat: add tast 2

--- multiarrays/script.js
+++ multiarrays/script.js
@@ -48,13 +48,29 @@
 // Сформируйте с помощью двух вложенных циклов следующий массив:
 // [[1, 2], [3, 4], [5, 6], [7, 8]]
 
-let arr = [];
-let counter = 1;
+// let arr = [];
+// let counter = 1;
 
+// for(let i = 0; i < 4; i++){
+//     arr[i] = [];
+//     for(let j = 0; j < 2; j++){
+//         arr[i].push(counter++);
+//     }
+// }
+
+// console.log(arr);
+
+// №2⊗jsPmMuAFO
+// Сформируйте с помощью двух вложенных циклов следующий массив:
+
+// [[2, 4, 6], [8, 10, 12], [14, 16, 18], [20, 22, 24]]
+
+let arr = [];
+let counter = 0;
 for(let i = 0; i < 4; i++){
     arr[i] = [];
-    for(let j = 0; j < 2; j++){
-        arr[i].push(counter++);
+    for(let j = 0; j < 3; j++){
+        arr[i].push(counter += 2);
     }
 }
 
