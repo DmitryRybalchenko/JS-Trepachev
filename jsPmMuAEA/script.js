1504f768ba26573dbcc420020d97c9ab6c604573
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-20 13:23:13 +1000

    feat: add jsPmMuAEA

---
+++ jsPmMuAEA/script.js
@@ -0,0 +1,29 @@
+// // №1⊗jsPmMuAEA
+
+// // Добавьте в следующий массив еще одного работника:
+
+// let employees = [
+// 	{
+// 		name: 'name1',
+// 		salary: 300,
+// 		age: 28,
+// 	},
+// 	{
+// 		name: 'name2',
+// 		salary: 400,
+// 		age: 29,
+// 	},
+// 	{
+// 		name: 'name3',
+// 		salary: 500,
+// 		age: 30,
+// 	},
+// ];
+
+// employees.push({
+//     name: 'Vasy',
+//     salary: 200,
+//     age: 18
+// })
+
+// console.log(employees);
