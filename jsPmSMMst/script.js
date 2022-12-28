ef31c2b3af06a426882b80ce023099168841c6fb
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-28 19:16:52 +1000

    feat: end jsPmSMMst

---
+++ jsPmSMMst/script.js
@@ -0,0 +1,70 @@
+// №1⊗jsPmSMMst
+// Код должен найти сумму цифр числа:
+
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let sum = 0;
+// for (let digit of arr) {
+// 	sum += +digit;
+// }
+
+// console.log(sum);
+
+// let num = 12345;
+// let sum = String(num).split('').reduce((total, elem) => {
+//   return total += +elem
+// }, 0);
+// console.log(sum);
+
+// №2⊗jsPmSMMst
+// Код должен найти сумму цифр числа:
+
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let sum = 0;
+// for (let digit of arr) {
+// 	sum += +digit;
+// }
+
+// console.log(sum);
+
+// №3⊗jsPmSMMst
+// Код должен найти сумму цифр числа:
+
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let sum = 0;
+// for (let digit of arr) {
+// 	sum += Number(digit);
+// }
+
+// console.log(sum); // почему-то выводит 5, а не 15
+
+// №4⊗jsPmSMMst
+// Код должен найти сумму цифр числа:
+
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let sum = 0;
+// for (let digit of arr) {
+// 	sum += Number(digit);
+// }
+
+// console.log(sum);
+
+// №5⊗jsPmSMMst
+// Код должен найти произведение цифр числа:
+
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let prod = 1;
+// for (let digit of arr) {
+// 	prod *= digit;
+// }
+
+// console.log(prod);
