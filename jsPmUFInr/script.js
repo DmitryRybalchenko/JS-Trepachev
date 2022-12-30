1fdd0f53252ced2bf8d3763303ff5f68411d73c1
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-30 16:40:07 +1000

    feat: end jsPmUFInr

---
+++ jsPmUFInr/script.js
@@ -0,0 +1,21 @@
+// №1⊗jsPmUFInr
+// Сделайте функцию, выводящую в консоль ваше имя.
+
+// function name (){
+//   console.log('Dim');
+// }
+
+// name();
+
+// №2⊗jsPmUFInr
+// Сделайте функцию, выводящую в консоль сумму чисел от 1 до 100.
+
+// function getSum(){
+//   let sum = 0;
+//   for(let i = 1; i <= 100; i++){
+//     sum += i;
+//   }
+//   console.log(sum);
+// }
+
+// getSum();
