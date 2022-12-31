830f13ad7e655b56b175f4c517b2ebfad747c253
Dmitry Rybalchenko <t9089700658@gmail.com>
2022-12-31 16:53:47 +1000

    feat: end jsPmUFSC

---
+++ jsPmUFSC/script.js
@@ -0,0 +1,33 @@
+// №1⊗jsPmUFSC
+// Пусть у вас есть функция, возвращающая квадратный корень из числа, и функция, округляющая дробь до трех знаков в дробной части:
+
+// function sqrt(num) {
+// 	return Math.sqrt(num);
+// }
+
+// function round(num) {
+// 	return num.toFixed(3);
+// }
+// С помощью этих функций найдите квадратный корень из числа 2 и округлите его до трех знаков в дробной части.
+
+// let res = round(sqrt(2));
+// console.log(res);
+
+// №2⊗jsPmUFSC
+// Пусть у вас есть функция, возвращающая квадратный корень из числа, и функция, возвращающая сумму трех чисел:
+
+// function sqrt(num) {
+// 	return Math.sqrt(num);
+// }
+
+// function sum(num1, num2, num3) {
+// 	return num1 + num2 + num3;
+// }
+
+// function round(num){
+//   return num.toFixed(3);
+// }
+// С помощью этих функций найдите сумму корней чисел 2, 3 и 4 и запишите ее в переменную res.
+
+// let res = round(sum(sqrt(2), sqrt(3), sqrt(4)));
+// console.log(res);
