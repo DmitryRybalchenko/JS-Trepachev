dd0272a8686a781424914ca5cdbaaa54593d744c
Dmitry Rybalchenko <t9089700658@gmail.com>
2023-01-06 20:12:05 +1000

    feat: end jsPmUFFI

---
+++ jsPmUFFI/script.js
@@ -0,0 +1,33 @@
+// №1⊗jsPmUFFl
+// Сделайте функцию, которая параметром будет принимать массив с числами, и проверять, что все элементы в этом массиве являются четными числами.
+
+// function isEvery(arr){
+//   let flag = true;
+//   for(let elem of arr){
+//     if(elem % 2 !== 0){
+//       return flag = false;
+//     }
+//   }
+//   return flag;
+// }
+
+// console.log(func([2,6,4]));
+
+// function isEvery(arr){
+//   return arr.every(item => item % 2 === 0);
+// }
+
+// console.log(isEvery([2,3,6]));
+
+// №3⊗jsPmUFFl
+// Сделайте функцию, которая параметром будет принимать массив и проверять, есть ли в этом массиве два одинаковых элемента подряд.
+// function func(arr){
+//   for(let i = 0; i < arr.length; i++){
+//     if(arr[i] === arr[i + 1]) {
+//       return true;
+//     };
+//   }
+//   return false;
+// }
+
+// console.log(func([1,2,4,4,5]));
